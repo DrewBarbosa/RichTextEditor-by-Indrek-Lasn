5d073b9917c1f94f49a521eb1ffae3ae46bb35cf
Endrew Barbosa <48924728+DrewBarbosa@users.noreply.github.com>
2020-02-25 18:10:18 -0300

    new features to format your text and dark theme

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Editor } from 'slate-react';
-import { BoldMark, ItalicMark,  } from './index';
 import { Value } from 'slate';
-
+import Icon from "react-icons-kit";
+import {bold} from "react-icons-kit/feather/bold";
+import {italic} from "react-icons-kit/feather/italic";
+import {code} from "react-icons-kit/feather/code";
+import {list} from "react-icons-kit/feather/list";
+import {underline} from "react-icons-kit/feather/underline";
+import{BoldMark, ItalicMark, FormatToolbar} from "./index";
 
 const InitialValue = Value.fromJSON({
 	document: {
@@ -15,7 +20,7 @@ const InitialValue = Value.fromJSON({
 						object :'text',
 						leaves:[
 							{
-								text: 'My first paragraph',
+								text: 'First fixed paragraph!',
 							},
 						],
 					},
@@ -36,20 +41,15 @@ export default class TextEditor extends Component {
 	};
 
 	onKeyDown = (e, change) => {
-		/*
-			we want all our commands to start with the user pressing ctrl,
-			if they don't--we cancel the action.
-		*/
-
+	
 		if (!e.ctrlKey) {
 			return;
 		}
 
 		e.preventDefault();
 
-		/* Decide what to do based on the key code... */
 		switch (e.key) {
-			/* When "b" is pressed, add a "bold" mark to the text. */
+			
 			case 'b': {
 				change.toggleMark('bold');
 				return true;
@@ -59,6 +59,22 @@ export default class TextEditor extends Component {
 				return true;
 			}
 
+			case 'c':{
+				change.toggleMark('code');
+				return true;
+			}
+
+			case 'l':{
+				change.toggleMark('list');
+				return true;
+			}
+
+			case 'u':{
+				change.toggleMark('underline');
+				return true;
+			}
+
+			
 			default: {
 				return;
 			}
@@ -74,23 +90,84 @@ export default class TextEditor extends Component {
 			case 'italic':
 				return <ItalicMark {...props} />;
 
+				case 'code':
+				return<code{...props.attributes} > {props.children}</code>;
+
+			case 'list':
+				return(
+					<ul {...props.attributes}>
+						<li>{props.children}</li>
+					</ul>
+				);
+
+			case 'underline':
+				return<u {...props.attributes}>{props.children}</u>;
+
 			default: {
 				return;
 			}
 		}
 	};
 
+	onMarkClick =(e, type) =>{
+		e.preventDefault();
+
+		const {value} = this.state;
+
+		const change= value.change().toggleMark(type);
+
+		this.onChange(change);
+	};
+
 	
 
 	render() {
 		return (
+			<Fragment>
+				<FormatToolbar>
+				<button 
+						onPointerDown={(e) => this.onMarkClick(e,'code')}
+						className="tooltip-icon-button"
+					>
+					<Icon icon={code}/>
+					</button>
+
+					<button 
+						onPointerDown={(e) => this.onMarkClick(e,'list')}
+						className="tooltip-icon-button"
+					>
+					<Icon icon={list}/>
+					</button>
+
+					<button 
+						onPointerDown={(e) => this.onMarkClick(e,'underline')}
+						className="tooltip-icon-button"
+					>
+					<Icon icon={underline}/>
+					</button>
+
+					<button 
+						onPointerDown={(e) => this.onMarkClick(e,'bold')}
+						className="tooltip-icon-button"
+					>
+					<Icon icon={bold}/>
+					</button>
+
+					<button 
+						onPointerDown={(e) =>this.onMarkClick(e,'italic')}
+						className="tooltip-icon-button"
+					>
+					<Icon icon={italic}/>
+					</button>
+				</FormatToolbar>
 				<Editor
 					value={this.state.value}
 					onChange={this.onChange}
 					onKeyDown={this.onKeyDown}
 					renderMark={this.renderMark}
-					
+					onMarkClick = {this.onMarkClick}
 				/>
+			</Fragment>
 		);
 	}
 }
